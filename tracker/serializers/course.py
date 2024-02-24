48711f5af3dec96b25b93e3656f546c73be4f07f
PolinaWednesday <kekssmail@gmail.com>
2024-02-24 12:30:20 +0300

    фильтрация, юзер, оплата и количество уроков.

--- tracker/serializers/course.py
+++ tracker/serializers/course.py
@@ -1,15 +1,15 @@
 from rest_framework import serializers
 from tracker.models import Course
+from tracker.serializers.lesson import LessonSerializer
 
 
 class CourseSerializer(serializers.ModelSerializer):
     lesson_count = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Course
-        fields = ['id', 'title', 'description', 'lesson_count']
+    lessons = LessonSerializer(many=True, read_only=True)
 
     def get_lesson_count(self, obj):
         return obj.lesson_set.count()
 
-
+    class Meta:
+        model = Course
+        fields = ['id', 'title', 'description', 'lesson_count', 'lessons']
