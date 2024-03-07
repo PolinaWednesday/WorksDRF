de9a75a4de9abb3b5b732dc220cc80b6447c24e7
PolinaWednesday <kekssmail@gmail.com>
2024-03-07 22:28:01 +0300

    Настройка прав доступа в DRF 2.0

--- tracker/models.py
+++ tracker/models.py
@@ -25,7 +25,6 @@ class Lesson(models.Model):
     images = models.ImageField(upload_to='tracker/', **NULLABLE, verbose_name='Превью')
     video_link = models.URLField(blank=True, verbose_name='Ссылка на видео')
     course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='lessons')
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, **NULLABLE, verbose_name='Пользователь')
 
     def __str__(self):
         return self.title
--- users/models.py
+++ users/models.py
@@ -6,7 +6,7 @@ NULLABLE = {'null': True, 'blank': True}
 
 class User(AbstractUser):
     username = None
-    email = models.EmailField(unique=True, **NULLABLE, verbose_name='Электронная почта')
+    email = models.EmailField(unique=True, default='', verbose_name='Электронная почта')
     phone = models.CharField(max_length=20, **NULLABLE, verbose_name='Номер телефона')
     city = models.CharField(max_length=100, **NULLABLE, verbose_name='Город')
     avatar = models.ImageField(upload_to='users/', **NULLABLE, verbose_name='Аватарка')
